a4362b240b0ca27d99cd2c5794af9acd6b7a29d3
Johannes Michael <janimc@gmail.com>
2018-09-22 16:28:42 -0700

    tests.js added

---
+++ main.js
@@ -0,0 +1,7 @@
+function sum(a, b) { //eslint-disable-line
+    var summed = a + b;
+    var concatenated = 'The sum of ' + a + ' and ' + b + ' is ' + summed + '.';
+    var returnArray = [summed, concatenated];
+    return returnArray;
+  }
+
